4b230e4f06315539c0807d8df2f6c7aacd719617
jaeeun <jaeeun9862@gmail.com>
2024-07-10 16:07:11 +0900

    MyPage 생성

---
+++ src/pages/Mypage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PlayLists from './PlayLists';
+import LikeLists from './LikeLists';
+
+const Mypage = () => {
+  return (
+    <div>
+      <PlayLists />
+      <LikeLists />
+    </div>
+  );
+};
+
+export default Mypage;
