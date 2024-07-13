8772241e2325eb2ccdd502bf84cee6cbf9c156cb
jaeeun <jaeeun9862@gmail.com>
2024-07-13 22:38:14 +0900

    PlayLists 기본 구조 작업

--- src/pages/PlayLists.jsx
+++ src/pages/PlayLists.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
+import CommonListsHeader from '../components/CommonListsHeader';
 
 const PlayLists = () => {
-  return <div>플레이리스트</div>;
+  const navigate = useNavigate();
+  const onCancel = () => {
+    navigate(-1);
+  };
+
+  const wrapStyle = { width: '360px', position: 'relative' };
+
+  return (
+    <div className='PlayLists' style={wrapStyle}>
+      <CommonListsHeader title={'플레이리스트'} />
+      <div className='PlayLists_listWrap'>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+      <IoIosArrowBack className='backBtn' onClick={onCancel} />
+    </div>
+  );
 };
 
 export default PlayLists;
