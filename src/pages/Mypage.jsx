b1e0b4d8691938e6844b8acb55299771450a2312
jaeeun <jaeeun9862@gmail.com>
2024-07-16 16:16:38 +0900

    UserInfo 가져오기 및 userImg, userName 설정

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoIosArrowBack } from 'react-icons/io';
 import { Link, useNavigate } from 'react-router-dom';
 import { BiSolidUser } from 'react-icons/bi';
 import { RiPlayListFill } from 'react-icons/ri';
 import { IoIosHeart } from 'react-icons/io';
 import '../styles/Mypage.css';
+import axios from 'axios';
 
 const Mypage = () => {
   const navigate = useNavigate();
@@ -12,13 +13,43 @@ const Mypage = () => {
     navigate(-1);
   };
 
+  const token = sessionStorage.getItem('token');
+  const [userName, setUserName] = useState('');
+  const [userID, setUserID] = useState('');
+  const [userImg, setUserImg] = useState('');
+
+  // 사용자 프로필 가져오기
+  const getUserInfo = async () => {
+    await axios
+      .get(`https://api.spotify.com/v1/me`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        const data = res.data;
+        setUserName(data.display_name);
+        setUserID(data.id);
+        setUserImg(data.images[1].url);
+      })
+      .catch((e) => {
+        console.log('Mypage getUserInfo 오류');
+      });
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
   return (
     <div className='Mypage'>
       <div className='Mypage_userWrap'>
         <div className='Mypage_userImg'>
-          <BiSolidUser className='icon_userImg' />
+          {userImg ? (
+            <img src={userImg} alt='userImg' />
+          ) : (
+            <BiSolidUser className='icon_userImg' />
+          )}
         </div>
-        <div className='Mypage_userName'>USER</div>
+        <div className='Mypage_userName'>{userName}</div>
       </div>
       <div className='Mypage_listWrap'>
         <Link to='/playlists'>
