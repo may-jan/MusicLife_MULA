312bdc63fcfd73371d04138d12e5f70501ff9525
jaeeun <jaeeun9862@gmail.com>
2024-07-18 15:46:12 +0900

    playlist 트랙 나열을 위한 구조 수정

--- src/components/CommonListsHeader.jsx
+++ src/components/CommonListsHeader.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { RiPlayListFill } from 'react-icons/ri';
 import { IoIosHeart } from 'react-icons/io';
+import TextFlow from '../utils/TextFlow';
 import '../styles/CommonListsHeader.css';
 
-const CommonListsHeader = ({ title }) => {
+const CommonListsHeader = ({ title, data }) => {
   return (
     <div className='CommonListsHeader'>
       {title === '플레이리스트' ? (
-        <>
-          <RiPlayListFill className='CommonListsHeader_icon' />
-          <div className='CommonListsHeader_title'>플레이리스트</div>
-        </>
+        !data ? (
+          <>
+            <RiPlayListFill className='CommonListsHeader_icon' />
+            <div className='CommonListsHeader_title'>플레이리스트</div>
+          </>
+        ) : (
+          <>
+            <div className='CommonListsHeader_title CommonListsHeader_listName text_overflow'>
+              <TextFlow text={data.plName} />
+            </div>
+            <div className='CommonListsHeader_listTotal'>{data.plTotal}곡</div>
+          </>
+        )
       ) : (
         <>
           <IoIosHeart className='CommonListsHeader_icon' />
