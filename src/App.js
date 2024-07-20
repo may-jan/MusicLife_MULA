d591672c9f092d0c69aa0b54989ba76284607330
jaeeun <jaeeun9862@gmail.com>
2024-07-20 16:39:33 +0900

    라우팅 이름 및 경로 변경

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Mypage from './pages/Mypage';
 import Swal from 'sweetalert2';
 import PlayLists from './pages/PlayLists';
 import LikeLists from './pages/LikeLists';
-import GetList from './hooks/GetList';
+import PlayListDetail from './pages/PlayListDetail';
 
 function App() {
   const [searchResults, setSearchResults] = useState({});
@@ -64,7 +64,7 @@ function App() {
             <Route path='/music/:id' element={<Music />} />
             <Route path='/mypage' element={<Mypage />} />
             <Route path='/playlists' element={<PlayLists />} />
-            <Route path='/playlists/:id' element={<GetList />} />
+            <Route path='/playlists/:id' element={<PlayListDetail />} />
             <Route path='/likelists' element={<LikeLists />} />
           </Routes>
         </BrowserRouter>
