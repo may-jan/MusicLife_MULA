58545517198af87edc332c259f4a986f50fcf6e0
jaeeun <jaeeun9862@gmail.com>
2024-07-25 17:26:31 +0900

    askAlert, resultAlert 적용

--- src/utils/removePlayList.js
+++ src/utils/removePlayList.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import askAlert from './askAlert';
+import resultAlert from './resultAlert';
 
 const RemovePlayList = async ({ playListID, albumUri, plSnapshotID }) => {
   const token = sessionStorage.getItem('token');
-  const remove = window.confirm('삭제하시겠습니까?');
+  const result = askAlert('삭제하시겠습니까?');
 
-  if (remove) {
+  if (await result) {
     try {
       const res = await axios.delete(
         `https://api.spotify.com/v1/playlists/${playListID}/tracks`,
@@ -22,19 +24,18 @@ const RemovePlayList = async ({ playListID, albumUri, plSnapshotID }) => {
           },
         }
       );
-
       // 성공적으로 삭제된 경우 PlayListDetail로 true 반환
-      alert('삭제되었습니다');
+      resultAlert('삭제되었습니다', 'success');
       return true;
     } catch (e) {
       // 삭제 실패한 경우
-      alert('삭제에 실패하였습니다');
+      resultAlert('삭제에 실패하였습니다', 'error');
       console.log('removePlayList 오류', e);
       return false;
     }
   } else {
     // 사용자가 취소한 경우
-    alert('취소되었습니다');
+    resultAlert('취소되었습니다', 'error');
     return false;
   }
 };
