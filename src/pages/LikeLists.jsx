fbdad55630d842840025845c586a04d9c462aaa5
jaeeun <jaeeun9862@gmail.com>
2024-07-28 20:48:46 +0900

    찜한 목록에서 삭제하는 기능 구현, 찜한 목록 데이터가 없는 경우 안내 메세지 설정

--- src/pages/LikeLists.jsx
+++ src/pages/LikeLists.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoIosArrowBack } from 'react-icons/io';
 import { IoIosHeart } from 'react-icons/io';
 import { Link, useNavigate } from 'react-router-dom';
 import CommonListsHeader from '../components/CommonListsHeader';
 import List from '../components/List';
-import askAlert from '../utils/askAlert';
-import resultAlert from '../utils/resultAlert';
 import GetLikeTracks from '../utils/getLikeTracks';
+import RemoveLikeTrack from '../utils/removeLikeTrack';
 import '../styles/PlayListDetail.css';
 
 const LikeLists = () => {
@@ -15,39 +14,61 @@ const LikeLists = () => {
     navigate(-1);
   };
 
-  const wrapStyle = { width: '360px', position: 'relative' };
+  const [likeLists, setLikeLists] = useState(null);
 
-  const likeTracks = GetLikeTracks();
+  useEffect(() => {
+    const fetchLikeTracks = async () => {
+      const data = await GetLikeTracks();
+      setLikeLists(data);
+    };
+    fetchLikeTracks();
+  }, []);
 
   // 하트 취소 버튼 누를 때 함수
-  const removeHeart = async () => {
-    const result = askAlert('찜한 목록에서 삭제하시겠습니까?');
-    if (await result) {
-      // 찜한 목록에서
-      // 삭제하는 코드
-      resultAlert('삭제되었습니다', 'success');
+  const removeHeart = async (e) => {
+    e.preventDefault();
+    const id = e.target.closest('.heart_removeBtn').id;
+    const result = await RemoveLikeTrack(id);
+
+    if (result) {
+      setLikeLists((prevList) => ({
+        tracks: prevList.tracks.filter((track) => track.track.id !== id),
+        total: prevList.total - 1,
+      }));
     } else {
-      resultAlert('취소되었습니다', 'error');
+      console.log('잘못된 클릭');
     }
   };
 
-  return (
-    <div className='LikeLists' style={wrapStyle}>
-      <CommonListsHeader title={'찜한 목록'} />
-      <div className='LikeLists_listWrap'>
-        {likeTracks &&
-          likeTracks.map((track) => (
-            <div key={`${track.track.id}`}>
-              <Link to={`/music/${track.track.id}`}>
-                <List data={track.track} />
-                <IoIosHeart className='heart_removeBtn' onClick={removeHeart} />
-              </Link>
-            </div>
-          ))}
-      </div>
+  const wrapStyle = { width: '360px', position: 'relative' };
 
-      <IoIosArrowBack className='backBtn' onClick={onCancel} />
-    </div>
+  return (
+    likeLists && (
+      <div className='LikeLists' style={wrapStyle}>
+        <CommonListsHeader title={'찜한 목록'} total={likeLists.total} />
+        <div className='LikeLists_listWrap'>
+          {likeLists.total !== 0 ? (
+            likeLists.tracks.map((track) => (
+              <div key={`${track.track.id}`}>
+                <Link to={`/music/${track.track.id}`}>
+                  <List data={track.track} />
+                  <IoIosHeart
+                    className='heart_removeBtn'
+                    onClick={removeHeart}
+                    id={track.track.id}
+                  />
+                </Link>
+              </div>
+            ))
+          ) : (
+            <div className='LikeLists_noList emptyData'>
+              찜한 목록이 존재하지 않습니다
+            </div>
+          )}
+        </div>
+        <IoIosArrowBack className='backBtn' onClick={onCancel} />
+      </div>
+    )
   );
 };
 
