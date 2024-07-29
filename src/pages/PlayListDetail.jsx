6054ed93a5fd633c362e18d79e10c10cdff894c6
jaeeun <jaeeun9862@gmail.com>
2024-07-29 23:48:30 +0900

    데이터 수정, 음악 데이터가 없는 경우 안내 메세지 설정, className 수정

--- src/pages/PlayListDetail.jsx
+++ src/pages/PlayListDetail.jsx
@@ -6,6 +6,7 @@ import List from '../components/List';
 import CommonListsHeader from '../components/CommonListsHeader';
 import GetPlayListInfo from '../utils/getPlayListInfo';
 import RemovePlayList from '../utils/removePlayList';
+import GetPlayListItems from '../utils/getPlayListItems';
 import '../styles/PlayListDetail.css';
 
 const PlayListDetail = () => {
@@ -16,13 +17,19 @@ const PlayListDetail = () => {
   };
 
   const [playListInfo, setPlayListInfo] = useState(null);
+  const [playListItems, setPlayListItems] = useState(null);
 
   useEffect(() => {
     const fetchPlayListInfo = async () => {
       const data = await GetPlayListInfo(id);
       setPlayListInfo(data);
     };
+    const fetchPlayListItems = async () => {
+      const data = await GetPlayListItems(id);
+      setPlayListItems(data);
+    };
     fetchPlayListInfo();
+    fetchPlayListItems();
   }, [id]);
 
   const removeMusic = async (e) => {
@@ -38,8 +45,7 @@ const PlayListDetail = () => {
         plSnapshotID,
       });
 
-      // RemovePlayList 함수에서 성공적으로 삭제된 경우
-      // 바뀐 PlTracks를 업데이트하여 UI 갱신
+      // RemovePlayList 함수에서 성공적으로 삭제된 경우 -> 바뀐 정보를 업데이트하여 UI 갱신
       if (result) {
         setPlayListInfo((prevInfo) => ({
           ...prevInfo,
@@ -47,6 +53,9 @@ const PlayListDetail = () => {
             (track) => track.track.uri !== albumUri
           ),
         }));
+        setPlayListItems(
+          playListItems.filter((li) => li.track.uri !== albumUri)
+        );
       }
     } else {
       console.log('잘못된 클릭');
@@ -57,30 +66,36 @@ const PlayListDetail = () => {
 
   return (
     playListInfo && (
-      <div className='GetList' style={wrapStyle}>
-        <div className='GetList_listHeader'>
+      <div className='PlayListDetail' style={wrapStyle}>
+        <div className='PlayListDetail_listHeader'>
           <CommonListsHeader
             title={'플레이리스트'}
             data={playListInfo}
             name={'PlayListDetail'}
           />
         </div>
-        <div className='GetList_listWrap'>
-          {playListInfo.plTracks.map((track, idx) => (
-            <div key={`${track.added_at}_${idx}`}>
-              <Link to={`/music/${track.track.id}`}>
-                <List data={track.track} playListID={id} />
-              </Link>
-              <HiOutlineMinusCircle
-                className='List_removeBtn'
-                onClick={removeMusic}
-                id={JSON.stringify({
-                  id: track.track.id,
-                  uri: track.track.uri,
-                })}
-              />
+        <div className='PlayListDetail_listWrap'>
+          {playListItems && playListItems.length !== 0 ? (
+            playListItems.map((item) => (
+              <div key={item.track.uri}>
+                <Link to={`/music/${item.track.id}`}>
+                  <List data={item.track} playListID={id} />
+                </Link>
+                <HiOutlineMinusCircle
+                  className='List_removeBtn'
+                  onClick={removeMusic}
+                  id={JSON.stringify({
+                    id: item.track.id,
+                    uri: item.track.uri,
+                  })}
+                />
+              </div>
+            ))
+          ) : (
+            <div className='PlayListDetail_noList emptyData'>
+              <p>음악이 존재하지 않습니다</p>
             </div>
-          ))}
+          )}
         </div>
         <IoIosArrowBack className='backBtn' onClick={onCancel} />
       </div>
