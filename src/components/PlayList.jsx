f4db712164a7210dafb249988803e29b1fdfd53d
jaeeun <jaeeun9862@gmail.com>
2024-07-29 23:47:12 +0900

    플레이리스트 데이터가 없는 경우 안내 메세지 설정

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -7,30 +7,36 @@ import GetPlayLists from '../utils/getPlayLists';
 const PlayList = () => {
   const playLists = GetPlayLists();
 
-  return (
-    playLists && (
-      <div className='PlayList'>
-        {playLists.map((playList) => {
-          return (
-            <Link to={playList.id} key={playList.id}>
-              <div className='PlayList_wrap'>
-                <div className='PlayList_img'>
+  return playLists && playLists.length !== 0 ? (
+    <div className='PlayList'>
+      {playLists.map((playList) => {
+        return (
+          <Link to={playList.id} key={playList.id}>
+            <div className='PlayList_wrap'>
+              <div className='PlayList_img'>
+                {playList.images === null ? (
+                  <div className='PlayList_noImg emptyImg'>NO IMAGE</div>
+                ) : (
                   <img src={playList.images[0].url} alt='img' />
+                )}
+              </div>
+              <div className='PlayList_listWrap'>
+                <div className='PlayList_listName text_overflow'>
+                  <TextFlow text={playList.name} />
                 </div>
-                <div className='PlayList_listWrap'>
-                  <div className='PlayList_listName text_overflow'>
-                    <TextFlow text={playList.name} />
-                  </div>
-                  <div className='PlayList_listTotal'>
-                    {playList.tracks.total}곡
-                  </div>
+                <div className='PlayList_listTotal'>
+                  {playList.tracks.total}곡
                 </div>
               </div>
-            </Link>
-          );
-        })}
-      </div>
-    )
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  ) : (
+    <div className='PlayList_noList emptyData'>
+      플레이리스트가 존재하지 않습니다
+    </div>
   );
 };
 
