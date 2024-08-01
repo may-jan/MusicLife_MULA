91f144bb6d446eeb8c4d61630647af1d35279833
jaeeun <jaeeun9862@gmail.com>
2024-08-01 22:53:45 +0900

    코드 구조 변경

--- src/utils/getPlayLists.js
+++ src/utils/getPlayLists.js
@@ -1,32 +1,18 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
 
-const GetPlayLists = () => {
+const GetPlayLists = async () => {
   const token = sessionStorage.getItem('token');
-  const [playListInfo, setPlayListInfo] = useState();
 
-  const getPlayLists = async () => {
-    await axios
-      .get(`https://api.spotify.com/v1/me/playlists`, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((res) => {
-        const data = res.data;
-        setPlayListInfo(data.items);
-      })
-      .catch((e) => {
-        console.log('getPlayLists fn 오류');
-      });
-  };
-
-  playListInfo &&
-    localStorage.setItem('playLists', JSON.stringify(playListInfo));
-
-  useEffect(() => {
-    getPlayLists();
-  }, []);
-
-  return playListInfo;
+  try {
+    const res = await axios.get(`https://api.spotify.com/v1/me/playlists`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = res.data;
+    return data.items;
+  } catch (e) {
+    console.log('getPlayLists fn 오류');
+    return null;
+  }
 };
 
 export default GetPlayLists;
