0c7eee59645539837dd767723be7f0c308fdc56a
jaeeun <mayjan1223@nextinnovation.co.kr>
2025-02-22 23:04:35 +0900

    refactor : 추천 플레이리스트 API 제공 중단으로 인한 로직 수정

--- src/components/RecommendPlaylists.jsx
+++ src/components/RecommendPlaylists.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/RecommendPlaylists.css';
-import GetFeaturedPlayLists from '../utils/getFeaturedPlayLists';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/RecommendPlaylists.css";
+import GetFeaturedPlayLists from "../utils/getFeaturedPlayLists";
 
 const RecommendPlaylists = () => {
-  const token = sessionStorage.getItem('token');
+  const token = sessionStorage.getItem("token");
   const playlists = GetFeaturedPlayLists();
 
   return token ? (
-    <div className='RecommendPlaylists'>
+    <div className="RecommendPlaylists">
       <h4>추천 플레이리스트 🎧</h4>
-      {playlists.map((list) => {
-        return (
-          <div className='RecommendPlaylist' key={list.id}>
-            <Link to={list.external_urls.spotify} target='_blank'>
-              <img src={list.images[0].url} alt='playlistCoverImg' />
-              <div className='RecommendPlaylists_playlistName'>{list.name}</div>
-              <div className='RecommendPlaylists_playlistDes'>
-                {list.description}
-              </div>
-            </Link>
-          </div>
-        );
-      })}
+      {playlists.length > 0 ? (
+        playlists.map((list) => {
+          return (
+            <div className="RecommendPlaylist" key={list.id}>
+              <Link to={list.external_urls.spotify} target="_blank">
+                <img src={list.images[0].url} alt="playlistCoverImg" />
+                <div className="RecommendPlaylists_playlistName">
+                  {list.name}
+                </div>
+                <div className="RecommendPlaylists_playlistDes">
+                  {list.description}
+                </div>
+              </Link>
+            </div>
+          );
+        })
+      ) : (
+        <div className="close_api">
+          추천 플레이리스트
+          <br />
+          API 제공이 중단되었습니다
+        </div>
+      )}
     </div>
   ) : (
-    <h4 className='loginRequest emptyData'>로그인을 먼저 진행해주세요</h4>
+    <h4 className="loginRequest emptyData">로그인을 먼저 진행해주세요</h4>
   );
 };
 
