658c74abd3976029ed5a451bedbce5eb57cd6ce6
jaeeun <mayjan1223@nextinnovation.co.kr>
2025-02-23 21:19:29 +0900

    fix : Spotify API에서 snapshot_id 없이 요청해야 정상 작동하는 것을 확인하여 snapshot_id 주석처리

--- src/utils/removePlayList.js
+++ src/utils/removePlayList.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-import askAlert from './askAlert';
-import resultAlert from './resultAlert';
+import axios from "axios";
+import askAlert from "./askAlert";
+import resultAlert from "./resultAlert";
 
 const RemovePlayList = async ({ playListID, albumUri, plSnapshotID }) => {
-  const token = sessionStorage.getItem('token');
-  const result = askAlert('플레이리스트에서 삭제하시겠습니까?');
+  const token = sessionStorage.getItem("token");
+  const result = askAlert("플레이리스트에서 삭제하시겠습니까?");
 
   if (await result) {
     try {
@@ -20,22 +20,22 @@ const RemovePlayList = async ({ playListID, albumUri, plSnapshotID }) => {
                 uri: albumUri,
               },
             ],
-            snapshot_id: plSnapshotID,
+            // snapshot_id: plSnapshotID,
           },
         }
       );
       // 성공적으로 삭제된 경우 PlayListDetail로 true 반환
-      resultAlert('삭제되었습니다', 'success');
+      resultAlert("삭제되었습니다", "success");
       return true;
     } catch (e) {
       // 삭제 실패한 경우
-      resultAlert('삭제에 실패하였습니다', 'error');
-      console.log('removePlayList 오류', e);
+      resultAlert("삭제에 실패하였습니다", "error");
+      console.log("removePlayList 오류", e);
       return false;
     }
   } else {
     // 사용자가 취소한 경우
-    resultAlert('취소되었습니다', 'error');
+    resultAlert("취소되었습니다", "error");
     return false;
   }
 };
